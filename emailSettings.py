656dded60a89171b28043f14936d19881b5395ca
milan <makanym@gmail.com>
2021-02-15 13:17:46 +0100

    Create emailSettings.py

---
+++ emailSettings.py
@@ -0,0 +1,16 @@
+# List of IMAP servers and ports bellow.
+# https://www.arclab.com/en/kb/email/list-of-smtp-and-imap-servers-mailserver-list.html
+SERVER = 'imap.gmail.com'
+PORT = 993
+
+# PERSONAL INFORMATION
+EMAIL_ADDRESS = ''
+PASSWORD = ''
+
+MAILBOX = r'[Gmail]/Starred'
+# MAILBOX = 'Inbox'
+
+EXPORT_LOCATION = r'C:\Users\makan\OneDrive\Desktop\Said\Mailbox\Renee-Editorial\emailData\\'
+
+FILTER_SENDER = False
+SENDER_ORGANISATION = ''
